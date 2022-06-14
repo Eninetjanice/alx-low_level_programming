2cb2668c4b11b5b4640862214125b07d0e5d2901
Janeth <enijaneth@gmail.com>
2022-06-13 21:58:23 -0700

    My commit for strings on a new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _puts - print a string followed by a new line to stdout.
+*@*str: string output
+*Return: No return
+*/
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i]);
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
