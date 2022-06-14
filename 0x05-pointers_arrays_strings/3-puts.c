52e5f19e9dc7c0b675243058f3acf4c972343125
Janeth <enijaneth@gmail.com>
2022-06-14 07:49:55 -0700

    Commit for C pointers task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,14 +2,14 @@
 
 /**
 * _puts - print a string followed by a new line to stdout.
-*@*str: string output
+*@str: string output
 *Return: No return
 */
 void _puts(char *str)
 {
 	int i = 0;
 
-	while (str[i]);
+	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
 		i++;
