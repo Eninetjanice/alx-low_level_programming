83a2bd28f0055fcb588fe8e6c66a05eec16922d4
Eninetjanice <71446962+Eninetjanice@users.noreply.github.com>
2022-08-11 05:18:03 +0100

    Updated lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -2,11 +2,13 @@
 #define _LISTS_H
 #include <stdio.h>
 #include <string.h>
+#include <stddef.h>
 #include <stdlib.h>
 /**
  * struct listint_s- singly linked list
  * @n:integer
  * @next: pointer to the node
+ *
  * Description: singly linked list node structure
  **/
 typedef struct listint_s
@@ -21,5 +23,10 @@ listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 void free_listint2(listint_t **head);
+int pop_listint(listint_t **head);
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+int sum_listint(listint_t *head);
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
 
 #endif
